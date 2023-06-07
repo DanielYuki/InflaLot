8737035679f28867fd0869509b2939ca465cb187
Daniel Yuki <danielyukihiga1@gmail.com>
2023-06-07 02:56:22 -0300

    Implementing basic structure

---
+++ front-end/src/pages/tickets_section/TicketsSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TicketsSection.css';
+
+export default function TicketsSection() {
+  return (
+    <div className="Tickets_Section">
+      <p>
+        Tickets_Section
+      </p>
+    </div>
+  );
+}
